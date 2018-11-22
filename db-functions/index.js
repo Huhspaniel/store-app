394481c8999358371a094208c6479985e10ca3c1
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-22 12:33:49 -0500

    Create addProducers and bulkCreateSkus methods in db-functions

--- db-functions/index.js
+++ db-functions/index.js
@@ -3,6 +3,34 @@ module.exports = function (db) {
         createProduct: createProduct,
         bulkCreateProducts: bulkCreateProducts
     };
+
+    function findDepartmentId(department) {
+        return new Promise((resolve, reject) => {
+            if (department && String(department).match(/[a-z]/i))
+                db.department.findOne({ where: { name: department } })
+                    .then(({ id }) => resolve(id)).catch(reject);
+            else resolve(department)
+        })
+    }
+    function addProducers(product_id, producer_ids) {
+        return db.producer.findAll({
+            where: db.Sequelize.or(
+                ...producer_ids.map(producer_id => ({ id: producer_id }))
+            )
+        }).then(producers => {
+            if (producers) {
+                for (let producer of producers) {
+                    producer.addProduct(product_id);
+                }
+            }
+        })
+    }
+    function bulkCreateSkus(skus, product_id) {
+        return db.sku.bulkCreate(skus.map(sku => {
+            sku.product_id = product_id;
+            return sku;
+        }), { individualHooks: true })
+    }
     function createProduct(product, variants) {
         const passedAttributes = variants.reduce((attributes, variant, i) => {
             attributes.push(...Object.entries(variant.attributes).map(
@@ -10,74 +38,52 @@ module.exports = function (db) {
             ));
             return attributes;
         }, []);
-        return new Promise((resolve, reject) => {
-            if (product.department && String(product.department).match(/[a-z]/i))
-                db.department.findOne({ where: { name: product.department } })
-                    .then(resolve).catch(reject);
-            else resolve({ id: product.department })
-        }).then(({ id: department_id }) => {
-            if (!product.department_id) product.department_id = department_id;
-            return db.product.create(product)
-        }).then(resProduct => {
-            const producer_ids = product.producer_ids;
-            product = resProduct;
-            if (producer_ids)
-                return db.producer.findAll({
-                    where: db.Sequelize.or(
-                        ...producer_ids.map(producer_id => ({ id: producer_id }))
-                    )
-                });
-        }).then(producers => {
-            if (producers) {
-                for (let producer of producers) {
-                    producer.addProduct(product);
-                }
-            }
-        }).then(() => {
-            if (variants) {
-                return db.sku.bulkCreate(variants.map(variant => {
-                    variant.product_id = product.id;
-                    return variant;
-                }), { individualHooks: true })
-            }
-        }).then(skus => {
-            return db.product_attribute.bulkCreate(
-                passedAttributes.map(({ name }) => ({ name: name, product_id: product.id }))
-                    .filter(({ name: currName }, i, arr) => {
-                        return arr.findIndex(({ name }) => name === currName) === i
-                    })
-            ).then(attributes => {
-                return db.product_attribute_value.bulkCreate(
-                    attributes.reduce((values, { id, name: currAttr }, i) => {
-                        values.push(
-                            ...passedAttributes.filter(({ name }) => name === currAttr)
-                                .map(({ value }) => ({ value: value, attribute_id: id }))
-                        )
-                        return values;
-                    }, [])
-                ).then(values => {
-                    return db.product_variant.bulkCreate(
-                        variants.reduce((variations, variant, i) => {
-                            variations.push(...Object.entries(variant.attributes).map(passedAttr => {
-                                const product_id = product.id;
-                                const sku_id = skus[i].id;
-                                const attribute_id = attributes.find(attr => attr.name === passedAttr[0]).id;
-                                const value_id = values.find(val => {
-                                    return val.attribute_id === attribute_id && val.value === passedAttr[1];
-                                }).id;
-                                return {
-                                    product_id: product_id,
-                                    sku_id: sku_id,
-                                    attribute_id: attribute_id,
-                                    value_id: value_id
-                                };
-                            }))
-                            return variations;
+        return findDepartmentId(product.department)
+            .then(department_id => {
+                product.department_id = product.department_id || department_id;
+                return db.product.create(product)
+            }).then(db_product => {
+                product.id = db_product.id;
+                if (product.producer_ids) return addProducers(product.id, product.producer_ids)
+            }).then(() => bulkCreateSkus(variants, product.id))
+            .then(skus => {
+                return db.product_attribute.bulkCreate(
+                    passedAttributes.map(({ name }) => ({ name: name, product_id: product.id }))
+                        .filter(({ name: currName }, i, arr) => {
+                            return arr.findIndex(({ name }) => name === currName) === i
+                        })
+                ).then(attributes => {
+                    return db.product_attribute_value.bulkCreate(
+                        attributes.reduce((values, { id, name: currAttr }, i) => {
+                            values.push(
+                                ...passedAttributes.filter(({ name }) => name === currAttr)
+                                    .map(({ value }) => ({ value: value, attribute_id: id }))
+                            )
+                            return values;
                         }, [])
-                    )
+                    ).then(values => {
+                        return db.product_variant.bulkCreate(
+                            variants.reduce((variations, variant, i) => {
+                                variations.push(...Object.entries(variant.attributes).map(passedAttr => {
+                                    const product_id = product.id;
+                                    const sku_id = skus[i].id;
+                                    const attribute_id = attributes.find(attr => attr.name === passedAttr[0]).id;
+                                    const value_id = values.find(val => {
+                                        return val.attribute_id === attribute_id && val.value === passedAttr[1];
+                                    }).id;
+                                    return {
+                                        product_id: product_id,
+                                        sku_id: sku_id,
+                                        attribute_id: attribute_id,
+                                        value_id: value_id
+                                    };
+                                }))
+                                return variations;
+                            }, [])
+                        )
+                    })
                 })
             })
-        })
     }
     function bulkCreateProducts(products) {
         const productsPromises = products.map(({ product, variants }) => createProduct(product, variants));
