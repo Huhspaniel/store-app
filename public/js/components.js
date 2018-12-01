a764963a9c895a34e2520f7126699177f955483d
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-12-01 15:24:47 -0500

    Display stock status

--- public/js/components.js
+++ public/js/components.js
@@ -1,17 +1,25 @@
-const Sku = props => (
-    `<div class=product key=${props.id}>
-        <h2>${props.name || props.product.name}</h2>
-        <h2>$${props.price || props.product.price}</h2>
-        <img src="${props.img_url || props.product.img_url}" alt="product image"/>
-    </div>`
-)
+const Sku = props => {
+    const goodStock = props.stock > 15;
+    return (
+        `<div class=product key=${props.id}>
+            <div>
+                <h2>${props.name || props.product.name}</h2>
+                <h2>$${props.price || props.product.price}</h2>
+                <h2 style="color:${goodStock ? 'green' : 'red'}">
+                    ${goodStock ? 'In Stock' : props.stock ? `Only ${props.stock} left in stock` : 'Out of Stock'}
+                </h2>
+            </div>
+            <img src="${props.img_url || props.product.img_url}" alt="product image"/>
+        </div>`
+    )
+}
 
 const AttributeSelect = props => (
     `<select name="${props.id}" key="${props.id}" value="${props.selected_value}">
         <option value="" disabled selected>Select ${props.name}</option>
         ${props.product_attribute_values.map(value => (
-            `<option value="${value.id}" key="${value.id}" 
+        `<option value="${value.id}" key="${value.id}" 
             ${value.value === props.selected_value ? 'selected' : ''}>${value.value}</option>`
-        )).join('')}
+    )).join('')}
     </select>`
 )
